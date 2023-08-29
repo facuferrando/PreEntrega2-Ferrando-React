a28f8df2bca1e6c86ac9b386187885cc4ae78ba8
Facundo Ferrando <facundoferrando96@gmail.com>
2023-08-29 19:17:42 -0300

    Agregando Logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,12 +2,14 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { CartWidget } from "./CartWidget";
+import { Logo } from "./Logo";
 export const NavBar =() => (
 
   <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="/">  </Navbar.Brand>
           <Nav className="me-auto">
+            <Nav.Link href="/"><Logo/> </Nav.Link>
             <Nav.Link href="/">Inicio</Nav.Link>
             <Nav.Link href="/category/remeras">Remeras</Nav.Link>
             <Nav.Link href="/category/buzos">Buzos</Nav.Link>
