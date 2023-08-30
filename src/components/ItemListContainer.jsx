d343cdcd50cdcdeb6cb145463bfeffb90917fc93
Facundo Ferrando <facundoferrando96@gmail.com>
2023-08-30 20:28:12 -0300

    correcciones

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -25,7 +25,7 @@ export const ItemListContainer = (props) => {
                 }
             });
           
-    }, []);
+    }, [id]);
     
 
     return (
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,17 +3,18 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { CartWidget } from "./CartWidget";
 import { Logo } from "./Logo";
+import { Link } from 'react-router-dom';
 export const NavBar =() => (
 
   <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="/">  </Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">  </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/"><Logo/> </Nav.Link>
-            <Nav.Link href="/">Inicio</Nav.Link>
-            <Nav.Link href="/category/remeras">Remeras</Nav.Link>
-            <Nav.Link href="/category/buzos">Buzos</Nav.Link>
-            <Nav.Link href="/category/pantalones">Pantalones</Nav.Link>
+            <Nav.Link as={Link} to="/"><Logo/> </Nav.Link>
+            <Nav.Link as={Link} to="/">Inicio</Nav.Link>
+            <Nav.Link as={Link} to="/category/remeras">Remeras</Nav.Link>
+            <Nav.Link as={Link} to="/category/buzos">Buzos</Nav.Link>
+            <Nav.Link as={Link} to="/category/pantalones">Pantalones</Nav.Link>
           </Nav> <CartWidget />
         </Container>
       </Navbar>
